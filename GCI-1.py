9d2cf926251e48a2c440b512bd414a0cd38a0603
ishan0412 <44732911+ishan0412@users.noreply.github.com>
2018-12-08 01:04:13 -0500

    Update GCI-1.py

--- GCI-1.py
+++ GCI-1.py
@@ -9,4 +9,5 @@ for _ in range(10):
 name = input("What's your name? ")
 name = list(name)
 revName = name[::-1]
+print("Hi " + name + ", nice to meet you! Did you know that your name spelled backwards is " + revName + "?")
 
